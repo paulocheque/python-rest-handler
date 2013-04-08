332a5e8bf51e22eb1fb2fc7a3cfb22083136f9c4
paulocheque <paulocheque@gmail.com>
2013-04-08 02:14:33 -0300

    [test-fix] missing tests from last commit

--- python_rest_handler/plugins/tests.py
+++ python_rest_handler/plugins/tests.py
@@ -6,18 +6,18 @@ from python_rest_handler.plugins.bootstrap import *
 
 class BootstrapWidgetsTests(unittest.TestCase):
     def test_input_text(self):
-        html = input_field('field')
+        html = bs_input_text('field')
         self.assertTrue('id="field"' in html)
 
     def test_input_password(self):
-        html = password_field('password')
+        html = bs_input_password('password')
         self.assertTrue('id="password"' in html)
 
     def test_input_select(self):
         options = [{'label':'Option 1', 'value':'value1'}, {'label':'Option 2', 'value':'value2'}]
-        html = select_field('choices', options, selected_value='value2')
+        html = bs_select_field('choices', options, selected_value='value2')
         self.assertTrue('id="choices"' in html)
 
     def test_button(self):
-        html = button(label='Add')
-        self.assertTrue('id="save"' in html)
+        html = bs_button(label='Add')
+        self.assertTrue('id="btn-save"' in html)
